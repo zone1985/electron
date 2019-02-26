553d469cafd15a7e00abf44801d515664834871f
John Kleinschmidt <jkleinsc@github.com>
2019-02-26 10:41:19 -0500

    WIP: reimplement PDF viewer

--- atom/browser/atom_resource_dispatcher_host_delegate.cc
+++ atom/browser/atom_resource_dispatcher_host_delegate.cc
@@ -18,8 +18,13 @@
 
 #if BUILDFLAG(ENABLE_PDF_VIEWER)
 #include "atom/common/atom_constants.h"
+#include "base/guid.h"
+#include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "content/public/browser/stream_info.h"
+#include "content/public/common/transferrable_url_loader.mojom.h"
+#include "extensions/browser/guest_view/mime_handler_view/mime_handler_stream_manager.h"
+#include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "net/url_request/url_request.h"
 #endif  // BUILDFLAG(ENABLE_PDF_VIEWER)
 
@@ -27,52 +32,12 @@ using content::BrowserThread;
 
 namespace atom {
 
-namespace {
-
-#if BUILDFLAG(ENABLE_PDF_VIEWER)
-void OnPdfResourceIntercepted(
-    const GURL& original_url,
-    int render_process_host_id,
-    int render_frame_id,
-    const content::ResourceRequestInfo::WebContentsGetter&
-        web_contents_getter) {
-  content::WebContents* web_contents = web_contents_getter.Run();
-  if (!web_contents)
-    return;
-
-  auto* web_preferences = WebContentsPreferences::From(web_contents);
-  if (!web_preferences || !web_preferences->IsEnabled(options::kPlugins)) {
-    auto* browser_context = web_contents->GetBrowserContext();
-    auto* download_manager =
-        content::BrowserContext::GetDownloadManager(browser_context);
-
-    download_manager->DownloadUrl(
-        content::DownloadUrlParameters::CreateForWebContentsMainFrame(
-            web_contents, original_url, NO_TRAFFIC_ANNOTATION_YET));
-    return;
-  }
-
-  // The URL passes the original pdf resource url, that will be requested
-  // by the webui page.
-  // chrome://pdf-viewer/index.html?src=https://somepage/123.pdf
-  content::NavigationController::LoadURLParams params(GURL(base::StringPrintf(
-      "%sindex.html?%s=%s", kPdfViewerUIOrigin, kPdfPluginSrc,
-      net::EscapeUrlEncodedData(original_url.spec(), false).c_str())));
-
-  content::RenderFrameHost* frame_host =
-      content::RenderFrameHost::FromID(render_process_host_id, render_frame_id);
-  if (!frame_host) {
-    return;
-  }
-
-  params.frame_tree_node_id = frame_host->GetFrameTreeNodeId();
-  web_contents->GetController().LoadURLWithParams(params);
-}
-#endif  // BUILDFLAG(ENABLE_PDF_VIEWER)
-
-}  // namespace
-
 AtomResourceDispatcherHostDelegate::AtomResourceDispatcherHostDelegate() {}
+AtomResourceDispatcherHostDelegate::~AtomResourceDispatcherHostDelegate() {
+#if BUILDFLAG(ENABLE_PDF_VIEWER)
+  CHECK(stream_target_info_.empty());
+#endif
+}
 
 bool AtomResourceDispatcherHostDelegate::ShouldInterceptResourceAsStream(
     net::URLRequest* request,
@@ -80,27 +45,88 @@ bool AtomResourceDispatcherHostDelegate::ShouldInterceptResourceAsStream(
     GURL* origin,
     std::string* payload) {
 #if BUILDFLAG(ENABLE_PDF_VIEWER)
-  content::ResourceRequestInfo* info =
-      content::ResourceRequestInfo::ForRequest(request);
-
-  int render_process_host_id;
-  int render_frame_id;
-  if (!info->GetAssociatedRenderFrame(&render_process_host_id,
-                                      &render_frame_id)) {
-    return false;
-  }
-
   if (mime_type == "application/pdf") {
+<<<<<<< HEAD
     *origin = GURL(kPdfViewerUIOrigin);
     base::PostTaskWithTraits(
         FROM_HERE, {BrowserThread::UI},
         base::BindOnce(&OnPdfResourceIntercepted, request->url(),
                        render_process_host_id, render_frame_id,
                        info->GetWebContentsGetterForRequest()));
+=======
+    StreamTargetInfo target_info;
+    *origin = GURL(base::StrCat({"chrome-extension://", kPdfExtensionId}));
+    target_info.extension_id = kPdfExtensionId;
+    target_info.view_id = base::GenerateGUID();
+    *payload = target_info.view_id;
+    stream_target_info_[request] = target_info;
+>>>>>>> f50e0b40f... WIP: reimplement PDF viewer
     return true;
   }
 #endif  // BUILDFLAG(ENABLE_PDF_VIEWER)
   return false;
 }
+
+void AtomResourceDispatcherHostDelegate::OnStreamCreated(
+    net::URLRequest* request,
+    std::unique_ptr<content::StreamInfo> stream) {
+#if BUILDFLAG(ENABLE_PDF_VIEWER)
+  content::ResourceRequestInfo* info =
+      content::ResourceRequestInfo::ForRequest(request);
+  auto ix = stream_target_info_.find(request);
+  CHECK(ix != stream_target_info_.end());
+  bool embedded = info->GetResourceType() != content::ResourceType::kMainFrame;
+  base::PostTaskWithTraits(
+      FROM_HERE, {content::BrowserThread::UI},
+      base::BindOnce(
+          &AtomResourceDispatcherHostDelegate::OnPdfResourceIntercepted,
+          ix->second.extension_id, ix->second.view_id, embedded,
+          info->GetFrameTreeNodeId(), info->GetChildID(),
+          info->GetRenderFrameID(), std::move(stream),
+          nullptr /* transferrable_loader */, GURL()));
+  stream_target_info_.erase(request);
+#endif
+}
+
+#if BUILDFLAG(ENABLE_PDF_VIEWER)
+void AtomResourceDispatcherHostDelegate::OnPdfResourceIntercepted(
+    const std::string& extension_id,
+    const std::string& view_id,
+    bool embedded,
+    int frame_tree_node_id,
+    int render_process_id,
+    int render_frame_id,
+    std::unique_ptr<content::StreamInfo> stream,
+    content::mojom::TransferrableURLLoaderPtr transferrable_loader,
+    const GURL& original_url) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+  content::WebContents* web_contents = nullptr;
+  if (frame_tree_node_id != -1) {
+    web_contents =
+        content::WebContents::FromFrameTreeNodeId(frame_tree_node_id);
+  } else {
+    web_contents = content::WebContents::FromRenderFrameHost(
+        content::RenderFrameHost::FromID(render_process_id, render_frame_id));
+  }
+  if (!web_contents)
+    return;
+
+  // If the mime handler uses MimeHandlerViewGuest, the MimeHandlerViewGuest
+  // will take ownership of the stream.
+  GURL handler_url(
+      GURL(base::StrCat({"chrome-extension://", extension_id})).spec() +
+      "index.html");
+  int tab_id = -1;
+  std::unique_ptr<extensions::StreamContainer> stream_container(
+      new extensions::StreamContainer(
+          std::move(stream), tab_id, embedded, handler_url, extension_id,
+          std::move(transferrable_loader), original_url));
+  auto* browser_context = web_contents->GetBrowserContext();
+  extensions::MimeHandlerStreamManager::Get(browser_context)
+      ->AddStream(view_id, std::move(stream_container), frame_tree_node_id,
+                  render_process_id, render_frame_id);
+}
+#endif  // BUILDFLAG(ENABLE_PDF_VIEWER)
 
 }  // namespace atom
--- atom/browser/atom_resource_dispatcher_host_delegate.h
+++ atom/browser/atom_resource_dispatcher_host_delegate.h
@@ -8,6 +8,11 @@
 #include <string>
 
 #include "content/public/browser/resource_dispatcher_host_delegate.h"
+#include "electron/buildflags/buildflags.h"
+
+#if BUILDFLAG(ENABLE_PDF_VIEWER)
+#include "content/public/common/transferrable_url_loader.mojom.h"
+#endif
 
 namespace atom {
 
@@ -15,6 +20,7 @@ class AtomResourceDispatcherHostDelegate
     : public content::ResourceDispatcherHostDelegate {
  public:
   AtomResourceDispatcherHostDelegate();
+  ~AtomResourceDispatcherHostDelegate() override;
 
   // content::ResourceDispatcherHostDelegate:
   bool ShouldInterceptResourceAsStream(net::URLRequest* request,
@@ -22,8 +28,33 @@ class AtomResourceDispatcherHostDelegate
                                        GURL* origin,
                                        std::string* payload) override;
 
+  void OnStreamCreated(net::URLRequest* request,
+                       std::unique_ptr<content::StreamInfo> stream) override;
+
+#if BUILDFLAG(ENABLE_PDF_VIEWER)
+  static void OnPdfResourceIntercepted(
+      const std::string& extension_id,
+      const std::string& view_id,
+      bool embedded,
+      int frame_tree_node_id,
+      int render_process_id,
+      int render_frame_id,
+      std::unique_ptr<content::StreamInfo> stream,
+      content::mojom::TransferrableURLLoaderPtr transferrable_loader,
+      const GURL& original_url);
+#endif
+
  private:
   DISALLOW_COPY_AND_ASSIGN(AtomResourceDispatcherHostDelegate);
+
+#if BUILDFLAG(ENABLE_PDF_VIEWER)
+  struct StreamTargetInfo {
+    std::string extension_id;
+    std::string view_id;
+  };
+
+  std::map<net::URLRequest*, StreamTargetInfo> stream_target_info_;
+#endif
 };
 
 }  // namespace atom
